d4cb8f1e2f3a7b37d004366f1a360b6057a06135
Diogo Costa <diogocosta.js@gmail.com>
2024-07-04 12:21:34 -0300

    Configuração de Tokens Sanctum + Início estrutura de Usuários

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Dotenv\Exception\ValidationException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException as ValidationValidationException;
+
+class UserController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+        // $request->validate([
+        //     'name' => 'required|string|max:255',
+        //     'email' => 'required|string|email|max:255|unique:users',
+        //     'password' => 'required|string|min:8|confirmed',
+        // ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['token' => $token, 'user' => $user]);
+    }
+
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['success' => false, 'message' => 'Usuário ou senha incorretos']);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['success' => true, 'token' => $token]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::post('/tokens/register', [UserController::class, 'register']);
+Route::post('/tokens/login', [UserController::class, 'login']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    die('lista');
+});
